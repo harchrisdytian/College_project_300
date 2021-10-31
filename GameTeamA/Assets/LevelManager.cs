ed575c7ff131dd3a94f42c0373e354577926dcf7
harchrisdytian <45524298+harchrisdytian@users.noreply.github.com>
2021-10-31 18:38:17 -0400

    pp

---
+++ GameTeamA/Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("Level Select")]
+    public int level = 0;
+
+    void Start()
+    {
+        
+    }
+
+}
